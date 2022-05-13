7172cbb95c6a488844a6a6b8fd44f609ea4c2679
fahimshahriar786 <fahimshahariar786@gmail.com>
2022-05-13 14:25:55 +0600

    import mongodb and jwt create port

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const app = express();
